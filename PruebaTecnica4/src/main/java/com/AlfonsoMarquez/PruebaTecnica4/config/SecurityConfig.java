1a522d7647d2fd5744f271871edc2cde9930132c
alfmargil <alfmargil@gmail.com>
2024-02-21 22:00:27 +0100

    Seguridad modificada y arreglada

--- PruebaTecnica4/src/main/java/com/AlfonsoMarquez/PruebaTecnica4/config/SecurityConfig.java
+++ PruebaTecnica4/src/main/java/com/AlfonsoMarquez/PruebaTecnica4/config/SecurityConfig.java
@@ -2,29 +2,34 @@ package com.AlfonsoMarquez.PruebaTecnica4.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/agency").permitAll()
+                // Permitir acceso sin autenticación a estas rutas
+                .requestMatchers("/agency/hotel-booking/new", "/agency/flight-booking/new").permitAll()
+                // Permitir acceso sin autenticación a los métodos GET
+                .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                // Requerir autenticación para el resto de las rutas
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
-                .and()
-                .httpBasic()
+                .formLogin().permitAll() // Permitir acceso al formulario de login
                 .and()
                 .build();
     }
-
-
 
 }
